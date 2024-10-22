ea8f1fdcf9c96dd0791058cc9980c47c0bfa3f05
programacion <damharnina25@iesarroyoharnina.es>
2024-10-22 09:54:50 +0200

    cola synchro

---
+++ Supermercado/src/synchro01/Cola.java
@@ -0,0 +1,31 @@
+package synchro01;
+
+import java.util.ArrayDeque;
+import java.util.Optional;
+
+import presentacion.Cliente;
+
+
+public class Cola {
+	public ArrayDeque<Cliente> clientes = new ArrayDeque<>();
+
+	public void push(Optional<Cliente> randomClient) {
+		randomClient.ifPresent((cliente) -> clientes.push(cliente));
+	}
+
+	public boolean isBigger(int i) {
+		return clientes.size() > 10;
+	}
+
+	public synchronized Cliente get() throws InterruptedException {
+		while (clientes.isEmpty()) {
+			wait();
+		}
+		return clientes.pop();
+	}
+
+	public synchronized void put(Optional<Cliente> optional) {
+		push(optional);
+		notifyAll();
+	}
+}
